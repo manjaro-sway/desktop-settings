8e11c8aaa256cff86ce9220e8be5bd1120f367b4
Chrysostomus <mattitheman@gmail.com>
2018-02-27 02:06:15 +0200

    Add rofi configuration

--- community/awesome/skel/.config/awesome/autorun.sh
+++ community/awesome/skel/.config/awesome/autorun.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 function run {
-  if ! pgrep $1 ;
-    then
-        $@&
-          fi
-          }
+  if ! pgrep $1 ; then
+    $@&
+  fi
+}
 run nm-applet
 run update-checker
 run light-locker
 run pulseaudio -D
 run pa-applet
 run compton -D
+run xcape -e 'Super_L=Super_L|Shift_L|space'
+run lxpolkit
