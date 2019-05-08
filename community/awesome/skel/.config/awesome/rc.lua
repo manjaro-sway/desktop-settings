506bb0e220d98ab13e075186880f63d3effa53f7
ThanosApostolou <thanosapostolou@outlook.com>
2019-05-08 15:52:04 +0300

    awesome: change close button to adapta too

--- community/awesome/skel/.config/awesome/rc.lua
+++ community/awesome/skel/.config/awesome/rc.lua
@@ -40,10 +40,13 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
+-- Chosen colors and buttons look alike adapta maia theme
 beautiful.init("/usr/share/awesome/themes/cesious/theme.lua")
 beautiful.icon_theme        = "Papirus-Dark"
 beautiful.bg_normal         = "#222D32"
 beautiful.bg_focus          = "#2C3940"
+beautiful.titlebar_close_button_normal = "/usr/share/awesome/themes/cesious/titlebar/close_normal_adapta.png"
+beautiful.titlebar_close_button_focus = "/usr/share/awesome/themes/cesious/titlebar/close_focus_adapta.png"
 beautiful.font              = "Noto Sans Regular 10"
 beautiful.notification_font = "Noto Sans Bold 14"
 
