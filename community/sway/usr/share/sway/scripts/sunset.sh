e313f5d609b62e7853aec51d983fba2a1dd3dbf8
Jonas Strassel <info@jonas-strassel.de>
2021-07-31 16:35:02 +0200

    feat: fallback to configured values

--- community/sway/usr/share/sway/scripts/sunset.sh
+++ community/sway/usr/share/sway/scripts/sunset.sh
@@ -12,14 +12,14 @@ function start(){
 		if [ -e $longitude ];
 		then
 			echo location ERROR: freegeoip.app
-			longitude='65'
-			latitude='60'
+			longitude="${longitude:-65}"
+			latitude="${latitude:-60}"
 		else
 			echo location OK:  $latitude $longitude
 		fi	
 		wlsunset -l $latitude -L $longitude -t $temp_low -T $temp_high -d $duration &
 	else
-		wlsunset -t $temp_low -T $temp_high  -d $duration -S $sunrise -s $sunset &
+		wlsunset -t $temp_low -T $temp_high -d $duration -S $sunrise -s $sunset &
 	fi
 }
 
