7c1512bb274d10a2f35a2f44d995de8458a39ce6
Jonas Strassel <info@jonas-strassel.de>
2021-07-31 16:53:26 +0200

    feat: make config file optional...
    ... by providing the defaults inside the script

--- community/sway/usr/share/sway/scripts/sunset.sh
+++ community/sway/usr/share/sway/scripts/sunset.sh
@@ -2,13 +2,21 @@
 
 #Startup function
 function start(){
-	source $HOME/.config/wlsunset/config
+	[[ -f "$HOME/.config/wlsunset/config" ]] && source "$HOME/.config/wlsunset/config"
+	temp_low=${temp_low:-"4000"}
+	temp_high=${temp_high:-"6500"}
+	duration=${duration:-"900"}
+	sunrise=${sunrise:-"07:00"}
+	sunset=${sunset:-"23:00"}
+	longitude=${longitude:-65}
+	latitude=${latitude:-65}
+	location=${location:-"off"}
 
-	if [ ${location} = "on" ]; 
+	if [ "${location}" = "on" ]; 
 	then
 		CONTENT=$(curl -s https://freegeoip.app/json/)
-		longitude=${$(echo $CONTENT | jq '.longitude // empty'):-${longitude:-65}}
-		latitude=${$(echo $CONTENT | jq '.latitude // empty'):-${latitude:-65}}
+		longitude=${$(echo $CONTENT | jq '.longitude // empty'):-"${longitude}"}
+		latitude=${$(echo $CONTENT | jq '.latitude // empty'):-"${latitude}"}
 		wlsunset -l $latitude -L $longitude -t $temp_low -T $temp_high -d $duration &
 	else
 		wlsunset -t $temp_low -T $temp_high -d $duration -S $sunrise -s $sunset &
