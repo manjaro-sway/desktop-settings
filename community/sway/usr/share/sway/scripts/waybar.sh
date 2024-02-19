866a864eac32edef4276a105042e86dcfa8b1de6
Michael Loveys <git@mloveys.dev>
2024-02-18 21:56:36 -0600

    Wait for xdg-desktop-portal to start before launching waybar

--- community/sway/usr/share/sway/scripts/waybar.sh
+++ community/sway/usr/share/sway/scripts/waybar.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 # wrapper script for waybar with args, see https://github.com/swaywm/sway/issues/5724
 
+WAYBAR_LOG=/tmp/waybar.$USER.log
+>$WAYBAR_LOG
+
 USER_CONFIG_PATH=$HOME/.config/waybar/config.jsonc
 USER_STYLE_PATH=$HOME/.config/waybar/style.css
+
+while ! pgrep -U $USER -f xdg-desktop-portal >/dev/null; do
+    date +"[%Y-%m-%d %T.%3N] Waiting for xdg-desktop-portal to start..." >> $WAYBAR_LOG
+    sleep 0.1
+done
 
 if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH
@@ -12,4 +20,4 @@ if [ -f "$USER_STYLE_PATH" ]; then
     USER_STYLE=$USER_STYLE_PATH
 fi
 
-waybar -c "${USER_CONFIG:-"/usr/share/sway/templates/waybar/config.jsonc"}" -s "${USER_STYLE:-"/usr/share/sway/templates/waybar/style.css"}" > $(mktemp -t XXXX.waybar.log) &
+waybar -c "${USER_CONFIG:-"/usr/share/sway/templates/waybar/config.jsonc"}" -s "${USER_STYLE:-"/usr/share/sway/templates/waybar/style.css"}" >> $WAYBAR_LOG &
