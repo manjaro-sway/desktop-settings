7acc1bd6957b70a436caf1b64bfa7e2d91d3e75e
ooxkf <52063318+ooxkf@users.noreply.github.com>
2025-03-29 15:45:19 +0700

    Update lock.sh to fix white background on lock-screen
    After a recent update, the lock screen displayed only a white background. I found that updating the lock screen using the listed options resolved the issue.
    
    Signed-off-by: ooxkf <52063318+ooxkf@users.noreply.github.com>

--- community/sway/usr/share/sway/scripts/lock.sh
+++ community/sway/usr/share/sway/scripts/lock.sh
@@ -5,5 +5,5 @@ if [ -x "$(command -v gtklock)" ]; then
 elif [ -x "$(command -v waylock)" ]; then
     waylock -fork-on-lock
 elif [ -x "$(command -v swaylock)" ]; then
-    swaylock --daemonize --show-failed-attempts
+    swaylock --daemonize --show-failed-attempts --screenshots --clock --indicator --effect-blur 7x5 --effect-vignette 0.5:0.5 --fade-in 0.2
 fi
