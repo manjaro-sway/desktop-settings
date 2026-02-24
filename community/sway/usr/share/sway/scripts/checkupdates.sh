296d7a54aeefe16e5e663a8e90be20ac0f1f7298
Jonas Strasel <info@jonas-strassel.de>
2026-02-25 00:08:35 +0100

    feat(sway): include AUR updates in waybar checkupdates script

--- community/sway/usr/share/sway/scripts/checkupdates.sh
+++ community/sway/usr/share/sway/scripts/checkupdates.sh
@@ -2,10 +2,10 @@
 
 case $1'' in
 'status') 
-    printf '{\"text\":\"%s\",\"tooltip\":\"%s\"}' "$(pamac checkupdates -q | wc -l)" "$(pamac checkupdates -q | awk 1 ORS='\\n' | sed 's/\\n$//')"
+    printf '{\"text\":\"%s\",\"tooltip\":\"%s\"}' "$(pamac checkupdates -q -a | wc -l)" "$(pamac checkupdates -q -a | awk 1 ORS='\\n' | sed 's/\\n$//')"
     ;;
 'check')
-    [ $(pamac checkupdates -q | wc -l) -gt 0 ]
+    [ $(pamac checkupdates -q -a | wc -l) -gt 0 ]
     exit $?
     ;;
 'upgrade')
