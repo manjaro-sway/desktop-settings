fbd3c8b52f88984dd544edc35cfee9a10f0ffbbb
Jonas Strassel <info@jonas-strassel.de>
2026-02-25 12:36:27 +0100

    fix(mako): don't pass sway theme args if user config exists Fixes #970

--- community/sway/usr/share/sway/scripts/mako.sh
+++ community/sway/usr/share/sway/scripts/mako.sh
@@ -3,7 +3,7 @@
 USER_CONFIG_PATH="${HOME}/.config/mako/config"
 
 if [ -f "$USER_CONFIG_PATH" ]; then
-    USER_CONFIG=$USER_CONFIG_PATH
+    exec mako -c "$USER_CONFIG_PATH"
+else
+    exec mako -c "/usr/share/sway/templates/mako" "$@"
 fi
-
-mako -c "${USER_CONFIG:-"/usr/share/sway/templates/mako"}" "$@"
